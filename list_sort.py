58fb9d213a31163f26096f9746070b86f6f6197e
StoneCats <112670109+schoolStoneCat@users.noreply.github.com>
2022-09-23 09:46:19 +0900

    Change Value and Code Fixed

--- list_sort.py
+++ list_sort.py
@@ -1,37 +1,37 @@
 def MakeList():
     list_max = int(input("List Max >>"))
     list = [0 for i in range(list_max)]
-    print("Finish 0 in List\n")
+    print("//Finish 0 in List\n")
     
     for i in range(list_max):
-        list[i] = int(input("list[{}] value >>".format(i)))
-        print("list value >>{}".format(list))
+        num = int(input("list[{}] value >>".format(i)))
         
-        # FIXME : 중복 체크 안됨
-        while list[i] in list:
-            print("ERROR >> Same num is exit in list plz Insert num again")
-            list[i] = int(input("list{} value >>".format(i)))
+        while num in list:
+            print("//ERROR >> Same num is exit in list plz Insert num again")
+            num = int(input("list[{}] value >>".format(i)))        
+        list[i] = num
+        print("list value >> {}".format(list))
                 
-    print("Finish Value in List")
-    
+    print("//Finish Value in List")   
     return list
 
 def EvenList(list):
     result_list = []
     
-    for i in len(list):
+    for i in range(0, len(list)):
         if(list[i] % 2 == 0):
             result_list.append(list[i])
-    
+    print("//Finish Analysis Even value")
     return result_list
 
 def OddList(list):
     result_list = []
     
-    for i in len(list):
+    for i in range(0, len(list)):
         if(list[i] % 2 != 0):
             result_list.append(list[i])
-            
+    
+    print("//Finish Analysis Odd value")  
     return result_list
 
 def AscendingList(result_list: list):
@@ -40,7 +40,7 @@ def AscendingList(result_list: list):
             if(result_list[i] > result_list[j]):
                 result_list[i], result_list[j] = result_list[j], result_list[i]
                 
-    print("Finish Ascentding List")
+    print("//Finish Ascentding List")
     return result_list
 
 def DescendingList(result_list: list):
@@ -49,13 +49,13 @@ def DescendingList(result_list: list):
             if(result_list[i] < result_list[j]):
                 result_list[i], result_list[j] = result_list[j], result_list[i]
                 
-    print("Finish Descentding List")
+    print("//Finish Descentding List")
     return result_list
     
 main_list = MakeList()
 
 print("Main List >> {}".format(main_list))
-print("{} List had only Even value".format(EvenList(main_list)))
-print("{} List had only odd value".format(OddList(main_list)))
-print("{} List is Ascending List".format(AscendingList(main_list)))
-print("{} List is Descending List".format(DescendingList(main_list)))
+print("Even value List >> {}".format(EvenList(main_list)))
+print("Odd value List >> {}".format(OddList(main_list)))
+print("Ascending List >> {}".format(AscendingList(main_list)))
+print("Descending List >> {}".format(DescendingList(main_list)))
