b76f6c0dd5f3df0b8e9ec78101a74276f1b86636
stonecat <apink191550@gmail.com>
2022-11-22 12:12:34 +0900

    Add New code User Number Analysis

--- Quick_sort.py
+++ Quick_sort.py
@@ -4,8 +4,16 @@ def Make_list(list):
     MAX_num = int(input("Insert Max num >>"))
     for i in range(1, MAX_num+1):
         list.append(i)
+
+##Change Code Error
+#def MakeList():
+    #list_max = int(input("List Max >>"))
+    #list = []
+    
+    #for i in range(list_max):
+    #    num = int(input("list[{}] value >>".format(i)))
         
-    random.shuffle(list)
+    #random.shuffle(list)
 
     print("//List >> {}".format(list))
     if len(list) == MAX_num:
@@ -39,3 +47,31 @@ list = []
 list = list_Quicksort(Make_list(list))
 
 print("List >> {}".format(list))
+
+def QuickSort_O(result_sort: list):
+    list_middle = int(random.randint(list))
+    i = 0
+    j = len(list) - 1
+    front_num = list[i]
+    last_num = list[j]
+    
+    while(last_num < front_num):
+        if(front_num > list_middle and last_num > list_middle):
+            j -= j
+        elif (front_num > list_middle and last_num < list_middle):
+            list[i], list[j] = list[j], list[i]
+            i += i
+            j -= j
+        elif (front_num < list_middle and last_num < list_middle):
+            i += i
+        elif (front_num < list_middle and last_num > list_middle):
+            i += i
+            j -= j
+    
+    middle_num = list.find(list_middle)
+    
+    for i in range(len(list)):
+        if(list[i] > list_middle):
+            list.remove(list_middle)
+            list.insert(i - 1, list_middle)
+
